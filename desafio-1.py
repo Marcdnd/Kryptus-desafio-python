55694f08e0cc6d75f563975e7459e3231cc38327
Marcdnd <marcio.denadai@gmail.com>
2020-07-22 16:49:18 -0300

    Correcao de duvida no exercicio

--- desafio-1.py
+++ desafio-1.py
@@ -8,13 +8,22 @@ last = lista[-1] # last = 3 {get last element }
 lista.append(8) # put 8
 tres = lista[2] # tres = 3 {get 3}
 dois = lista[1] # dois = 7 {get 2}
-print(lista) # (2, 7, 3, 8)
-lista.pop(2) # remove 3o element
+print(lista) # [2, 7, 3, 8]
+lista.pop(2) # remove 3º element
 print(lista) # [2, 7, 8]
 
 #test
 print(first,last,dois,tres) #  2 3 7 3
 
 lista = [] # clear
-exit()
+
+# Nota
+# Caso queira remover o elemento "2" use o comando
+# lista.remove(2)
+# no caso do exercicio do desafio talves eu tenha confundido a posicao do elemento com o elemento
+# entao a linha 12 ficaria assim
+# lista.remove(3) 
+# e a lista final seria => lista = [2, 7, 8]
+
+exit() # exit
 
