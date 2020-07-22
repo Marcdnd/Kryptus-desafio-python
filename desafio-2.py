ec1b2638e7510fdfd5f7537ce2137f5b2ae7c8dd
Marcdnd <marcio.denadai@gmail.com>
2020-07-22 15:40:34 -0300

    Desafio 2

---
+++ desafio-2.py
@@ -0,0 +1,11 @@
+#python
+lista=[] # inicia variavel lista
+lista.append(2) # put 2
+lista.append(7) # put 7
+lista.append(3) # put 3 
+lista.append(8) # put 8
+print(lista) # [2, 7, 3, 8]
+lista.sort() # ordenacao de lista
+print(lista) # [2, 3, 7, 8]
+exit()
+
